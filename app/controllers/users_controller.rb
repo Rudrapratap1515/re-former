866ba786082a9cea3966cc707da757ca798fbf0f
Rudrapratap <rudrapratap.gkmit.co>
2021-02-11 18:49:27 +0530

    created edit page and update feature in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+    before_action :set_blog, only: %i[edit update]
     def new
         @user = User.new
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to new_user_path
+        else
+            render :new
+        end
     end
     
     def create
@@ -15,6 +27,10 @@ class UsersController < ApplicationController
     end
 
     private
+
+    def set_blog
+        @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:name,:email,:passowrd)

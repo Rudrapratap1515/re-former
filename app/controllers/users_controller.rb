bbf201bc4fe0626fae58d09942c38ae86f854864
Rudrapratap <rudrapratap.gkmit.co>
2021-02-11 17:47:58 +0530

    created tag form for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.new(user_params)
+        @user = User.new(name:params[:name],email:params[:email],passowrd:params[:passowrd])
+        #@user = User.new(user_params)
 
         if @user.save
             redirect_to new_user_path
         else 
-            #render :new
+            render :new
         end
     end
 
